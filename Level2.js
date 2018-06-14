8bd45c16a615d165d016e4ccb723c42c88772e3f
dmoor5 <36045753+dmoor5@users.noreply.github.com>
2018-06-14 09:55:05 -0400

    Update Level2.js

--- Level2.js
+++ Level2.js
@@ -1,4 +1,13 @@
- function setup() {
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+
+}
+
+function draw() {
+	ellipse(mouseX, mouseY, 20, 20);
+}
+function setup() {
 createCanvas(windowWidth, windowHeight)
 	background(100);
 
@@ -10,33 +19,32 @@ fill(175,224,224);
 };
 var drawCrystalBottom = function(b,c,d,e){
 fill(175,224,224);
+	triangle(b,463,c,463,d,e);
 };
 draw = function() {
    
 
    background(107, 113, 115);
  
-   
-drawCrystalTop(0,87,44,84)
+    fill(66, 62, 62);
+ rect(0,333,400,129);
+	rect(470,333,400,129);
+		drawCrystalTop(0,87,44,84)
     drawCrystalTop(53,152,114,138);
-   drawCrystalTop(237,160,204,96);
-	drawCrystalTop(390,276,285,105);
-	
-fill(66, 62, 62);
-rect(0,333,400,129);
+  	drawCrystalTop(237,160,204,96);
+		drawCrystalTop(390,276,285,105);
+			drawCrystalBottom(158,74,109,338);
+			drawCrystalBottom(274,157,178,270);
+			drawCrystalBottom(18,89,40,338);
+			drawCrystalBottom(391,311,345,340);
+			drawCrystalBottom(313,239,264,340);
+			drawCrystalBottom(470,650,554,325);
+			drawCrystalBottom(577,700,750,350);
 // Crysals
 //triangle(309,0,226,0,250,106);
 
 fill(175, 224, 224);
-//triangle(0,0,87,0,44,84);
-//triangle(53,0,152,0,114,138);
-//triangle(257,0,160,0,204,96);
-// triangle(390,0,276,0,285,105);
-// triangle(158,400,74,400,109,338);
-// triangle(274,400,157,400,178,270);
-// triangle(18,407,89,400,40,338);
-// triangle(391,400,311,407,345,340);
-// triangle(313,400,239,400,264,340);
+
 //Character
 fill(62, 207, 110);
 ellipse(57,301,25,26);
@@ -46,8 +54,5 @@ rect(258,-15,10,222);
 rect(340,-15,10,222);
 rect(259,197,90,10);
 
-
 };
-
-
 
