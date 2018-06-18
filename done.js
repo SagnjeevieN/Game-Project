7760d8f8b7211d198dc277ec085a69ce48edb218
Ksenia K <36044703+Kkoro1@users.noreply.github.com>
2018-06-17 23:55:09 -0400

    Update done.js

--- done.js
+++ done.js
@@ -1,12 +1,10 @@
 var currentScene;
-
-
+function setup(){
+    createCanvas(400, 400);
     var grassPic = function(x, y, w, h) {
         var getGrass = loadImage("HEART-01.png");
         image(getGrass, x, y, w, h); 
     };
-function setup(){
-    createCanvas(400, 400);
     drawScene1();
 }
 
